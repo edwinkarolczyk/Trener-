68bb679544f4b32bfc0b4e41004f0f204342dadc
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-22 09:55:11 +0200

    feat: load approved diet design in 0.8.9.0

--- app/src/main/assets/feature-loader.js
+++ app/src/main/assets/feature-loader.js
@@ -44,6 +44,7 @@
     ['0.8.8.6','v0886-meal-sets.js'],
     ['0.8.8.6','v0886-food-entry.js'],
     ['0.8.8.9','v0889-meal-time.js'],
+    ['0.8.9.0','v0890-diet-visual.js'],
     ['0.8.5','v085-drag-layout.js'],
     ['0.8.6','v086-one-phone.js'],
     ['0.8.7','v087-nearby.js'],
